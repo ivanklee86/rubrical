ae0eb0e1194f82eacf8f4f7c6899a8e2c5ce6e77
Ivan Lee <ivanklee86@gmail.com>
2023-02-17 20:16:23 -0500

    Add configuration framework and comparisons (#16)
    * Add basic config and comparison operator.
    
    * Fix linting.
    
    * Add codecov.yaml.

--- rubrical/configuration/dependency_requirement.py
+++ rubrical/configuration/dependency_requirement.py
@@ -1,14 +1,43 @@
-from pydantic import BaseModel
+from typing import Optional
 
-from rubrical.enum import ComparisonOperators
+import semver
+from pydantic import BaseModel, PrivateAttr
+
+from rubrical.enum import DependencyCheck, DependencyTypes, SemverComparison
 
 
 class DependencyRequirement(BaseModel):
-    name: str
-    comparison: ComparisonOperators
+    type: Optional[DependencyTypes] = DependencyTypes.SEMVER
     warn: str
     block: str
+    _semver_warn: semver.VersionInfo = PrivateAttr
+    _semver_block: semver.VersionInfo = PrivateAttr
 
-    def silly_function(self):
-        print(self.name)
-        print(self.comparison)
+    def __init__(self, **data) -> None:
+        super().__init__(**data)
+
+        if type == DependencyTypes.SEMVER:
+            self._semver_warn = semver.VersionInfo.parse(self.warn)
+            self._semver_block = semver.VersionInfo.parse(self.block)
+
+    def check_dependency(self, dependency) -> DependencyCheck:
+        if type == DependencyTypes.SEMVER:
+            dependency_semver = semver.VersionInfo.parse(dependency.version)
+            warn_signal = SemverComparison.GT != semver.compare(
+                dependency_semver, self._semver_warn
+            )
+            block_signal = SemverComparison.GT != semver.compare(
+                dependency_semver, self._semver_block
+            )
+        else:
+            warn_signal = self.warn >= dependency.version
+            block_signal = self.block >= dependency.version
+
+        if block_signal:
+            status = DependencyCheck.BLOCK
+        elif warn_signal:
+            status = DependencyCheck.WARN
+        else:
+            status = DependencyCheck.OK
+
+        return status
