934bd482303ddfde4a517803a56047da7b86b5e4
Ivan Lee <ivanklee86@gmail.com>
2023-03-13 07:00:47 +0000

    Make reporting clearer.

--- rubrical/reporters/terminal.py
+++ rubrical/reporters/terminal.py
@@ -11,20 +11,24 @@ from rubrical.utilities import terminal
 def terminal_report(
     package_manager_name: str, check_results: List[PackageCheckResult]
 ) -> None:
-    console = Console()
+    # If any unsuccessful checks.
+    if not [x for x in check_results if x.check != PackageCheck.OK]:
+        terminal.print_message(f"[bold][dark_orange]{package_manager_name}[/dark_orange][/bold] checks completed with violations!")
 
-    terminal.print_message(f"Result for {package_manager_name}")
+        console = Console()
+        
+        table = Table("File", "Dependency", "Result")
+        
+        # Only report unsuccessful checks.
+        for result in [x for x in check_results if x.check == PackageCheck.OK] :
+            if result.check == PackageCheck.BLOCK:
+                result_text = f"❌ {result.version_package} <= {result.version_block}, update to > {result.version_warn}"
+            elif result.check == PackageCheck.WARN:
+                # Terminal is cranky about the emoji, needs two spaces.
+                result_text = f"⚠️  {result.version_package} <= {result.version_warn}"
 
-    table = Table("File", "Dependency", "Result")
-    for result in check_results:
-        if result.check == PackageCheck.BLOCK:
-            result_text = f"❌ {result.version_package} <= {result.version_block}, update to > {result.version_warn}"
-        elif result.check == PackageCheck.WARN:
-            # Terminal is cranky about the emoji, needs two spaces.
-            result_text = f"⚠️  {result.version_package} <= {result.version_warn}"
-        else:
-            result_text = "✅"
+            table.add_row(result.file, result.name, result_text)
 
-        table.add_row(result.file, result.name, result_text)
-
-    console.print(table)
+        console.print(table)
+    else:
+        terminal.print_message(f"[bold][spring_green1]{package_manager_name}[/spring_green1][/bold] checks completed with no warnings or blocks!")
