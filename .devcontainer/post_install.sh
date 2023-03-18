33c21ba14abd2fa1902510cf293280ed6c30ee99
Ivan Lee <ivanklee86@gmail.com>
2023-03-18 03:11:25 -0400

    Add release automation. (#35)
    * Add release automation.
    
    * License and classifiers.

--- .devcontainer/post_install.sh
+++ .devcontainer/post_install.sh
@@ -5,9 +5,18 @@ set -ex
 WORKSPACE_DIR=$(pwd)
 poetry config cache-dir ${WORKSPACE_DIR}/.cache
 poetry config virtualenvs.in-project true
+poetry self add "poetry-dynamic-versioning[plugin]"
 
 # Intall dependencies
 poetry install
+
+# Install pre-config
+pip install pre-commit
+pre-commit install
+pre-commit
+
+# Configure git
+git config --global --add --bool push.autoSetupRemote true
 
 # Woohoo!
 echo "Hooray, it's done!"
