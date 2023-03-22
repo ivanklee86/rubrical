c1bfa0f1543e0a0587f405cc8a850f5effa19cb8
Ivan Lee <ivanklee86@gmail.com>
2023-03-22 13:10:41 +0000

    Fail safe in cases folks are using branches.

--- rubrical/comparison.py
+++ rubrical/comparison.py
@@ -13,18 +13,22 @@ def check_package(
             package.version[1:] if package.version.startswith("v") else package.version
         )
 
-        warn_signal = SemverComparison.GT.value != semver.compare(
-            package_semver,
-            package_requirement.warn[1:]
-            if package_requirement.warn.startswith("v")
-            else package_requirement.warn,
-        )
-        block_signal = SemverComparison.GT.value != semver.compare(
-            package_semver,
-            package_requirement.block[1:]
-            if package_requirement.block.startswith("v")
-            else package_requirement.block,
-        )
+        try:
+            warn_signal = SemverComparison.GT.value != semver.compare(
+                package_semver,
+                package_requirement.warn[1:]
+                if package_requirement.warn.startswith("v")
+                else package_requirement.warn,
+            )
+            block_signal = SemverComparison.GT.value != semver.compare(
+                package_semver,
+                package_requirement.block[1:]
+                if package_requirement.block.startswith("v")
+                else package_requirement.block,
+            )
+        except ValueError:
+            warn_signal = False
+            block_signal = False
     else:
         warn_signal = package_requirement.warn >= package.version
         block_signal = package_requirement.block >= package.version
