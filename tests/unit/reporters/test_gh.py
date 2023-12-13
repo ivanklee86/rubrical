a75d4833c02c2763a59ae2f29df81d369daf9c0f
Ivan Lee <ivanklee86@gmail.com>
2023-12-13 04:09:31 -0500

    Clean up Github Rubrical comment if no issues are found. (#190)
    * Clean up Rubrical comment if no issues are found.
    
    * Dependency updates.

--- tests/unit/reporters/test_gh.py
+++ tests/unit/reporters/test_gh.py
@@ -45,6 +45,19 @@ DUMMY_RESULTS = {
     ],
 }
 
+DUMMY_CLEAN_RESULTS = {
+    "jsonnet": [
+        PackageCheckResult(
+            name="one",
+            file="file1.txt",
+            check=PackageCheck.OK,
+            version_package="1.1.1",
+            version_block="1.1.2",
+            version_warn="1.1.4",
+        )
+    ]
+}
+
 
 def test_gh_report_contents():
     text = gh._generate_report(DUMMY_RESULTS)
@@ -104,3 +117,45 @@ def test_gh_report(secrets, github_pr_clean):  # noqa: F811
             comment_count += 1
 
     assert comment_count == 1
+
+
+def test_gh_cleanup(secrets, github_pr_clean):  # noqa: F811
+    pr = github_pr_clean
+
+    # Test creation.
+    gh.report_github(
+        access_token=os.getenv("RUBRICAL_TEST_GITHUB_ACCESS_TOKEN"),
+        custom_url="",
+        repository_name=GITHUB_REPO_NAME,
+        pr_id=GITHUB_TEST_PR,
+        reporting_data=DUMMY_RESULTS,
+        warnings_found=True,
+        blocks_found=True,
+    )
+
+    # Check comment's created.
+    comment_count = 0
+    for issue_comment in pr.get_issue_comments():
+        if "Rubrical" in issue_comment.body:
+            comment_count += 1
+
+    assert comment_count == 1
+
+    # Test cleanup
+    gh.report_github(
+        access_token=os.getenv("RUBRICAL_TEST_GITHUB_ACCESS_TOKEN"),
+        custom_url="",
+        repository_name=GITHUB_REPO_NAME,
+        pr_id=GITHUB_TEST_PR,
+        reporting_data=DUMMY_CLEAN_RESULTS,
+        warnings_found=False,
+        blocks_found=False,
+    )
+
+    # Check there's still only one comment.
+    comment_count = 0
+    for issue_comment in pr.get_issue_comments():
+        if "Rubrical" in issue_comment.body:
+            comment_count += 1
+
+    assert comment_count == 0
