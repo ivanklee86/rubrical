1a18eb9f5861e8aefb7848274edb5b2528c007c7
Ivan Lee <ivanklee86@gmail.com>
2024-02-08 23:10:58 +0000

    Refactor taskfiles and prep for docs.

--- .devcontainer/post_install.sh
+++ .devcontainer/post_install.sh
@@ -16,6 +16,15 @@ pre-commit install
 pre-commit
 
 # Configure git
+if [ "$CODESPACES" != "true" ]; then
+    echo "Running locally, configure gpg."
+
+    git config --global gpg.program gpg2
+    git config --global commit.gpgsign true
+else
+    echo "Running in codespaces."
+fi
+
 git config --global --add --bool push.autoSetupRemote true
 
 # Woohoo!
